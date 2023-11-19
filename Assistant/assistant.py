79a201eebf5a34f1ff8638f67186fa97c857ebfa
Caleb Williamson <caleb7981@tamu.edu>
2023-11-18 23:57:21 -0600

    Added tests and implemented polling for assistant response.

--- Assistant/assistant.py
+++ Assistant/assistant.py
@@ -1,37 +1,46 @@
 from openai import OpenAI
-import json
-import os
+import os, time, json
 
 # Effectively creates a thread for the user to communicate with the assistant
 class CavClone():
     def __init__(self, client: OpenAI) -> None:
         self.client = client
         self.assistant_id = ""
+        self.thread_id = ""
 
-        # TODO: Test if this creates a new json file in the cwd
         if not os.path.exists(os.path.join(os.getcwd(), "assistant.json")):
-            with open("assistant.json", "w"):
+            with open("assistant.json", "w") as f:
+                print(f"File created.")
                 pass
         # Set/Get assistant id
+        print(f"Retrieving assistant...")
         if assistantExists(openai_client):
             with open("assistant.json", "r") as f:
                 self.assistant_id = json.load(f)
+            print(f"Assistant loaded from json file with id: {self.assistant_id}")
         else:
             self.assistant_id = createAssistant(openai_client)
+            print(f"Assistant created with id: {self.assistant_id}")
 
         # Create thread
+        print(f"Creating thread...")
         openai_client = self.client
         thread = openai_client.beta.threads.create()
         self.thread_id = thread.id
+        print(f"Thread created successfully with id: {self.thread_id}")
 
     def send_message(self, message: str) -> str:
         openai_client = self.client
         try:
+            # Store message in thread so that assistant can answer
+            print(f"Storing message...")
             message_object = openai_client.beta.threads.messages.create(
                 self.thread_id,
                 role="user",
-                conten=message 
+                content=message 
             )
+            # Execute call in order for assistant to process and answer the message
+            print(f"Message run started...")
             message_run = openai_client.beta.threads.runs.create(
                 thread_id=self.thread_id,
                 assistant_id=self.assistant_id
@@ -40,17 +49,45 @@ class CavClone():
             print("MESSAGE SENT")
             print("Waiting for assistant response...")
 
-            # TODO: Poll for run status and return response once completed or an error if one occurs
+            # Poll for run status
+            print(f"Polling for assistant response...")
+            run_status_response = openai_client.beta.threads.runs.retrieve(
+                thread_id=self.thread_id,
+                run_id=run_id
+            )
+
+            count = 0
+            while(True):
+                time.sleep(15)
+                count = count + 1
+                print(f"Attempt {count}...")
+                run_status_response = openai_client.beta.threads.runs.retrieve(
+                    thread_id=self.thread_id,
+                    run_id=run_id
+                )
+                print(f"Run Status Response: {run_status_response.status}")
+                if run_status_response.status == "completed":
+                    print(f"Response detected.")
+                    break
 
         except Exception as e:
             print("MESSAGE FAILED TO SEND")
             print("Error Details:")
             print(f"{e}")
 
+        # Display response to the console
+        message_list = openai_client.beta.threads.messages.list(
+        thread_id=self.thread_id
+        )
+        response_message = message_list.data[0].content[0].text.value
+        print(f"Message: {response_message}")
+
     def __del__(self) -> None:
         # Delete Thread (May need to be changed later for simplification)
+        print(f"Deleting thread...")
         openai_client = self.client
         response = openai_client.beta.threads.delete(self.thread_id)
+        print(f"Thread with id: {self.thread_idthread_id} deleted succesfully.")
 
 
 
@@ -75,7 +112,10 @@ def createAssistant(openai_client: OpenAI) -> str:
 def assistantExists(openai_client: OpenAI) -> bool:
     my_assistant = ""
     try:
-        my_assistant = openai_client.beta.assistants.retrieve("asst_abc123")
+        if os.path.exists(os.path.join(os.getcwd(), "assistant.json")):
+            with open("assistant.json", "r") as f:
+                id = json.load(f)
+        my_assistant = openai_client.beta.assistants.retrieve(id)
     except Exception as e:
         my_assistant = "Not Found"
 
---
+++ Assistant/tests.py
@@ -0,0 +1,133 @@
+from openai import OpenAI
+from dotenv import dotenv_values
+import os, json, time
+from assistant import assistantExists, createAssistant
+
+environment_variables = dotenv_values()
+openai_api_key = environment_variables['OPENAI_API_KEY']
+
+openai_client = OpenAI(api_key=openai_api_key)
+
+# Test assistant api by walking through the following steps
+# create assistant -> create thread -> create message -> run thread -> poll for response -> display response
+
+
+# Test 1 without OOP 
+##############################################################################################
+
+my_assistant_id = ""
+my_thread_id = ""
+
+if not os.path.exists(os.path.join(os.getcwd(), "assistant.json")):
+    with open("assistant.json", "w") as f:
+        print(f"File created.")
+        pass
+# Set/Get assistant id
+print(f"Retrieving assistant...")
+if assistantExists(openai_client):
+    with open("assistant.json", "r") as f:
+        my_assistant_id = json.load(f)
+    print(f"Assistant loaded from json file with id: {my_assistant_id}")
+else:
+    my_assistant_id = createAssistant(openai_client)
+    print(f"Assistant created with id: {my_assistant_id}")
+
+# Create Thread
+print(f"Creating thread...")
+thread = openai_client.beta.threads.create()
+my_thread_id = thread.id
+print(f"Thread created successfully with id: {my_thread_id}")
+
+# Delete Thread
+print(f"Deleting thread...")
+response = openai_client.beta.threads.delete(my_thread_id)
+print(f"Thread with id: {my_thread_id} deleted succesfully.")
+
+################################################################################################
+
+# Test 2 without OOP
+################################################################################################
+
+my_assistant_id = ""
+my_thread_id = ""
+
+if not os.path.exists(os.path.join(os.getcwd(), "assistant.json")):
+    with open("assistant.json", "w") as f:
+        print(f"File created.")
+        pass
+# Set/Get assistant id
+print(f"Retrieving assistant...")
+if assistantExists(openai_client):
+    with open("assistant.json", "r") as f:
+        my_assistant_id = json.load(f)
+    print(f"Assistant loaded from json file with id: {my_assistant_id}")
+else:
+    my_assistant_id = createAssistant(openai_client)
+    print(f"Assistant created with id: {my_assistant_id}")
+
+# Create Thread
+print(f"Creating thread...")
+thread = openai_client.beta.threads.create()
+my_thread_id = thread.id
+print(f"Thread created successfully with id: {my_thread_id}")
+
+message = "Howdy! What is 5 + 10?"
+
+try:
+    # Store message in thread so that assistant can answer
+    print(f"Storing message...")
+    message_object = openai_client.beta.threads.messages.create(
+        my_thread_id,
+        role="user",
+        content=message 
+    )
+    print(f"Message Object: {message_object}")
+    # Execute call in order for assistant to process and answer the message
+    print(f"Message run started...")
+    message_run = openai_client.beta.threads.runs.create(
+        thread_id=my_thread_id,
+        assistant_id=my_assistant_id
+    )
+    print(f"Message Run: {message_run}")
+    run_id = message_run.id
+    print("MESSAGE SENT")
+    print("Waiting for assistant response...")
+
+    # Poll for run status
+    print(f"Polling for assistant response...")
+    count = 0
+    while(True):
+        time.sleep(15)
+        count = count + 1
+        print(f"Attempt {count}...")
+        run_status_response = openai_client.beta.threads.runs.retrieve(
+            thread_id=my_thread_id,
+            run_id=run_id
+        )
+        print(f"Run Status Response: {run_status_response.status}")
+        if run_status_response.status == "completed":
+            print(f"Response detected.")
+            break
+
+except Exception as e:
+    print("MESSAGE FAILED TO SEND")
+    print("Error Details:")
+    print(f"{e}")
+
+# Display response to the console
+message_list = openai_client.beta.threads.messages.list(
+  thread_id=my_thread_id
+)
+response_message = message_list.data[0].content[0].text.value
+print(f"Message: {response_message}")
+
+# Delete Thread
+print(f"Deleting thread...")
+response = openai_client.beta.threads.delete(my_thread_id)
+print(f"Thread with id: {my_thread_id} deleted succesfully.")
+
+##################################################################################################
+
+
+
+
